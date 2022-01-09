1b75f4694f30a130cd3e513961612157ab79fb7d
SaiPraneethPegada <saipraneethpegada@gmail.com>
2022-01-09 20:36:53 +0530

    First commit

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ request.open('GET','https://raw.githubusercontent.com/rvsp/restcountries-json-da
 request.send();
 request.onload=function(){
     var data = JSON.parse(request.response);
-    //console.log(data);
-    for (var i = 0; i<data.length; i++){
-            console.log(data[i].name, data[i].region, data[i].subregion, data[i].population);
-    }
+    console.log(data);
+    // for (var i = 0; i<data.length; i++){
+    // console.log(data[i].name, data[i].region, data[i].subregion, data[i].population);
+        let s1= data.reduce((acc,ele)=>acc+ele.population,0)
+        console.log(s1);
 }
